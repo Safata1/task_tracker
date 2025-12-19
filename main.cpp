d5b189d7fb89963a314cb8f996f69562537bc9c7
Safa <safomoslih@gmail.com>
2025-12-19 12:36:40 +0100

    Get add/list/update/delete working

--- main.cpp
+++ main.cpp
@@ -1,26 +1,128 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <ctime>
+
+using namespace std ;
+struct Task {
+    int id;
+    string description;
+    string status;
+    string createdAt;
+    string updatedAt;
+};
+
+string getCurrentTime() {
+    time_t now = time(nullptr);
+    return string(ctime(&now)); 
+}
 
 int main() {
-    std::string command;
+    string command;
+    vector<Task> tasks;
+    int nextId = 1;
 
-    std::cout << "Welcome to Taskly CLI!\n";
-    std::cout << "Type 'help' to see available commands.\n";
+
+    cout << "Welcome to Task CLI!\n";
+    cout << "Type 'help' to see available commands.\n";
 
     while (true) {
-        std::cout << "> ";          // CLI prompt
-        std::getline(std::cin, command);
+        cout << "> ";          // CLI prompt
+        getline(cin, command);
 
         if (command == "exit") {
-            std::cout << "Goodbye!\n";
+            cout << "Goodbye!\n";
             break;
         } else if (command == "help") {
-            std::cout << "Available commands:\n";
-            std::cout << "add <task>    - Add a new task\n";
-            std::cout << "list          - List all tasks\n";
-            std::cout << "delete <id>   - Delete a task by ID\n";
-            std::cout << "exit          - Exit the application\n";
-        } else {
+            cout << "Available commands:\n";
+            cout << "add <task>    - Add a new task\n";
+            cout << "list          - List all tasks\n";
+            cout << "delete <id>   - Delete a task by ID\n";
+            cout << "exit          - Exit the application\n";
+        }
+         
+        //add task
+        else if (command.rfind("add ", 0) == 0) {
+            // Extract task description
+            std::string description = command.substr(4);
+
+            if (description.empty()) {
+                std::cout << "Error: task description cannot be empty.\n";
+            } else {
+                Task task;
+                task.id = nextId++;
+                task.description = description;
+                task.status = "todo";
+                task.createdAt = getCurrentTime();
+                task.updatedAt = task.createdAt;
+
+                tasks.push_back(task);
+
+                cout << "Task added (ID " << task.id << ")\n";
+            }
+
+        } 
+
+        //list task 
+        else if (command == "list") {
+            if (tasks.empty()) {
+                cout << "No tasks found.\n";
+            } else {
+                for (const auto& task : tasks) {
+                    cout << "[" << task.id << "] "
+                        << task.description
+                        << " | " << task.status << "\n";
+                }
+            }
+        }
+
+        //delete task 
+        else if (command.rfind("delete ", 0) == 0) {
+        int id = stoi(command.substr(7));
+
+        bool found = false;
+        for (auto it = tasks.begin(); it != tasks.end(); ++it) {
+            if (it->id == id) {
+                tasks.erase(it);
+                cout << "Task deleted.\n";
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            cout << "Task not found.\n";
+        }
+        }
+
+        //Update task
+        else if (command.rfind("update ", 0) == 0) {
+            string rest = command.substr(7);
+            size_t spacePos = rest.find(' ');
+
+            if (spacePos == string::npos) {
+                cout << "Usage: update <id> <status>\n";
+            } else {
+                int id = stoi(rest.substr(0, spacePos));
+                string newStatus = rest.substr(spacePos + 1);
+
+                bool found = false;
+                for (auto& task : tasks) {
+                    if (task.id == id) {
+                        task.status = newStatus;
+                        task.updatedAt = getCurrentTime();
+                        cout << "Task updated.\n";
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    cout << "Task not found.\n";
+                }
+            }
+        }
+        else {
             std::cout << "Unknown command. Type 'help' for a list of commands.\n";
         }
     }
